9b97fecb0b381c8580f2f22bc34f7c760427af01
vocaltech <it@vocaltech.fr>
2022-03-06 15:47:56 +0100

    Rename

--- src/test/java/fr/vocaltech/spring/statemachine/BasicStatemachineApplicationTests.java
+++ src/test/java/fr/vocaltech/spring/statemachine/BasicStatemachineApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class StatemachineApplicationTests {
+class BasicStatemachineApplicationTests {
 
 	@Test
 	void contextLoads() {
